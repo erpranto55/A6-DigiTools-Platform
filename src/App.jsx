18a06cdd1e6321220e0a661cddb751b22df7012e
erpranto <erpranto55@gmail.com>
2026-03-30 20:01:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import NavBar from './components/Navbar/NavBar'
+
+
+function App() {
+
+
+  return (
+    <>
+      <nav>
+        <h1>hellp</h1>
+
+      </nav>
+    </>
+  )
+}
+
+export default App
